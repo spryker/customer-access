8adf3c0f332121d89b43361780da33b303b80b73
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Shared/CustomerAccess/CustomerAccessConfig.php
+++ src/Spryker/Shared/CustomerAccess/CustomerAccessConfig.php
@@ -10,26 +10,36 @@ namespace Spryker\Shared\CustomerAccess;
 interface CustomerAccessConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTENT_TYPE_PRICE = 'price';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTENT_TYPE_ORDER_PLACE_SUBMIT = 'order-place-submit';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTENT_TYPE_ADD_TO_CART = 'add-to-cart';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTENT_TYPE_WISHLIST = 'wishlist';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const CONTENT_TYPE_SHOPPING_LIST = 'shopping-list';
